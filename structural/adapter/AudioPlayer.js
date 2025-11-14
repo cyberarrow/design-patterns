407f4413d642cc8dd5f0c0350f62652009aef124
beatriz-cantilho <bcantilho@protonmail.com>
2025-11-14 15:15:51 -0300

    using adapter pattern to solve incompatibility

---
+++ structural/adapter/AudioPlayer.js
@@ -0,0 +1,7 @@
+class AudioPlayer {
+    playAudio() {
+        throw new Error("Método playAudio() deve ser implementado")
+    }
+}
+
+module.exports = AudioPlayer
---
+++ structural/adapter/testeVideoPlayer.js
@@ -0,0 +1,5 @@
+const videoPlayerAdapter = require('./VideoPlayerAdapter');
+
+const player = new videoPlayerAdapter()
+
+player.playAudio("Heavy_is_the_crown.mp3")
