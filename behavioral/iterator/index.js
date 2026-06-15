3877f8ffca0bb5f741f20bf5a50e9d0a9e8727ba
cyberarrow <bcantilho@protonmail.com>
2026-06-15 18:29:37 -0300

    feat: apply new functions to products collection example

--- behavioral/iterator/index.js
+++ behavioral/iterator/index.js
@@ -1,4 +1,5 @@
 const UsersCollection = require('./UsersCollection')
+const ProductsCollection = require('./ProductsCollection')
 
 const users = new UsersCollection();
 users.addUser({ name: "Jon", age: 17 })
@@ -6,8 +7,27 @@ users.addUser({ name: "Sandor", age: 27 })
 
 const userIterator = users.createIterator(this.users);
 
-console.log("Iterate users: ")
+/*console.log("Iterate users: ")
 
 while(userIterator.hasNext()) {
     console.log(userIterator.next())
+}*/
+
+const products = new ProductsCollection();
+products.addProduct({ name: 'Freezer', value: 4000})
+products.addProduct({ name: 'Washing machine', value: 1800})
+products.addProduct({ name: 'Blender', value: 500})
+
+const productIterator = products.createIterator();
+
+console.log("Iterate products: ")
+
+while(productIterator.hasNext()) {
+    console.log(productIterator.next())
+}
+
+console.log("Iterate products - reverse: ")
+
+while(productIterator.hasPrevious()) {
+    console.log(productIterator.previous())
 }
