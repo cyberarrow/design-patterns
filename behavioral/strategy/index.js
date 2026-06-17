fee437a7c4bcb9adda5ec2ce4382e5fe3b6aebf7
cyberarrow <bcantilho@protonmail.com>
2026-06-16 21:10:23 -0300

    feat: applying strategy pattern

--- behavioral/strategy/index.js
+++ behavioral/strategy/index.js
@@ -1,5 +1,7 @@
 const Navigator = require("./models/Navigator");
+const BikeRoute = require("./models/BikeRoute");
 
 const navigator = new Navigator();
 
+navigator.setStrategy(new BikeRoute());
 navigator.calculateRoute("Brasília", "Lisboa");
